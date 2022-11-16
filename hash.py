7b004b0ed3ae843f308d66597b96c4e6c9a25b21
William L <41084946+wlinds@users.noreply.github.com>
2022-11-16 02:24:41 +0100

    spelling and comments

--- hash.py
+++ hash.py
@@ -3,22 +3,21 @@ import pandas as pd
 import hashlib
 
 def hash_team(df, country: str) -> pd.Series:
-    """Hashes column as series, values must be strings."""
+    """Returns column with hashed ahtlete names as pd.series."""
 
-    # Might need some error and exception handling,
-    # this should convert to string
-    
+    # Might need some error and exception handling, this should convert to string and works for now:    
     df["Team"] = df["Team"].astype(str)
 
     # Slice country parameter from DataFrame
     df_GER = df.loc[df["Team"] == country]
 
-    # Labda function for hashing
+    # Lambda function for encoding, hashing and digest hex
     return df_GER.iloc[:,1].apply(
         lambda x: hashlib.sha256(x.encode()).hexdigest())
 
 if __name__ == "__main__":
-
+    
+    # Testing function
     df = pd.read_csv("Data/athlete_events.csv")
 
     country = "Germany"
