394d72568722cf5d884126252511b723c3828bc6
William L <41084946+helveticae@users.noreply.github.com>
2022-11-17 03:01:26 +0100

    import hash_region from hash.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pandas as pd
-import re
+from hash import hash_region
 
+# testing
 
-# test
+# first data file
+df = pd.read_csv("Data/athlete_events.csv", usecols=['Name', 'Age', 'Sex', 'Team', 'NOC','Games','Year','Sport','Medal'])
+df.head()
+# second data file
+df1 = pd.read_csv("Data/noc_regions.csv",usecols=['region', 'NOC']) #### NOC: National Olympic Committee
+df1.head() 
+# merge both the files with corresconding columns
+df_merge = df1.merge(df, on="NOC",how = "left")
 
+country = "Germany"
+hashed_series = hash_region(df_merge, country) # Replace columns
+print(hashed_series.head())
 
-# test 2
-
-# test 3
