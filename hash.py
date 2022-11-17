7f1f8897900da1f490ba1b6eb2c8c19004857012
William L <41084946+helveticae@users.noreply.github.com>
2022-11-17 03:00:14 +0100

    support for merged columns (region)

--- hash.py
+++ hash.py
@@ -1,23 +1,10 @@
 from __future__ import annotations
 import pandas as pd
 import hashlib
-# first data file
-df = pd.read_csv("../Project/OlympicsData/athlete_events.csv", usecols=['Name', 'Age', 'Sex', 'Team', 'NOC','Games','Year','Sport','Medal'])
-df.head()
 
-# second data file
-df1 = pd.read_csv("../Project/OlympicsData/noc_regions.csv",usecols=['region', 'NOC']) #### NOC: National Olympic Committee
-df1.head() 
-# merge both the files with corresconding columns
-df_merge = df1.merge(df, on="NOC",how = "left")
-df_merge.head()
-
-
-
-def hash_team(df_merge, country: str) -> pd.Series:
+def hash_region(df_merge, country: str) -> pd.Series:
     """Returns column with hashed ahtlete names as pd.series."""
 
-    # Might need some error and exception handling, this should convert to string and works for now:    
     #df_merge["region"] = df_merge["region"].astype(str)
 
     # Slice country parameter from DataFrame
@@ -27,6 +14,22 @@ def hash_team(df_merge, country: str) -> pd.Series:
     return df_germany.iloc[:,2].apply(
         lambda x: hashlib.sha256(x.encode()).hexdigest())
 
-country = "Germany"
 
-hash_team(df_merge, country)
+if __name__ == "__main__":
+    # first data file
+    df = pd.read_csv("Data/athlete_events.csv", usecols=['Name', 'Age', 'Sex', 'Team', 'NOC','Games','Year','Sport','Medal'])
+    df.head()
+
+    # second data file
+    df1 = pd.read_csv("Data/noc_regions.csv",usecols=['region', 'NOC']) #### NOC: National Olympic Committee
+    df1.head() 
+    # merge both the files with corresconding columns
+    df_merge = df1.merge(df, on="NOC",how = "left")
+    df_merge.head()
+
+
+    country = "Iceland" # test lol
+
+    hashed_series = hash_region(df_merge, country) # Replace columns
+
+    print(hashed_series.head())
