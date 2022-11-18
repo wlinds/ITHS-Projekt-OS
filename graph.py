f2694d2c4f8445e6b1acaadb60f18f8f6fa28fb3
William L <41084946+helveticae@users.noreply.github.com>
2022-11-18 13:48:47 +0100

    plan graph functions

--- graph.py
+++ graph.py
@@ -1,1 +1,27 @@
+
+
 # as discusssed
+# functions to be imported into main.py
+
+#TODO
+
+# ------ Land statistics: ----- #
+
+#### Top 10 medals in germany
+# Barplot
+
+### Age distubution
+# Histogram
+
+### # Top 10 in Germany  (Women in sports)
+#Subtitle # Female participating throughout history (Ger)
+
+
+
+# ----- Sport statistics: ----- #
+
+# [Medals]
+# Dropdown as 
+
+# Medals dropdown ->  Select sport.
+
