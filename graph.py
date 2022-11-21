0402ff1b4770167be4811b76b3aaeec086b695b5
William L <41084946+helveticae@users.noreply.github.com>
2022-11-21 13:33:38 +0100

    recover lost comments

--- graph.py
+++ graph.py
@@ -7,4 +7,26 @@ import pandas as pd
 df_germany = df_medals[df_medals["region"] == "Germany"]
 print(df_germany)
 
-# What happened to the notes??? Did I remove them? /wil
+# ------ Land statistics: ----- #
+
+#### Top 10 medals in germany
+# Barplot
+
+### Age distubution
+# Histogram
+
+### # Top 10 in Germany  (Women in sports) # Women empowerment
+#Subtitle # Female participating throughout history (Ger)
+
+
+
+# ----- Sport statistics: ----- #
+
+# Football (country/region)
+# Dropdown -> Top Medals by country
+#          -> Number of persons from country represented in sport
+#          ->
+
+# Athletics 
+
+# Cross country skiing 
